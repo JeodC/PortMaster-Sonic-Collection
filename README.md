bf7180e750431973ba26899bfa037d871e58dca4
JeodC <47716344+JeodC@users.noreply.github.com>
2024-08-21 07:31:27 -0400

    Update binaries, add soniccd

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-## Sonic 1 & 2 -- PortMaster
-This repository houses Sonic 1 and Sonic 2 ports, with updated RSDKv4 built on 8/20/24, along with the mods Sonic Forever and Sonic 2 Absolute.
+## Sonic 1 & 2 & CD -- PortMaster
+This repository houses Sonic 1, Sonic 2, and Sonic CD ports, with updated RSDKv3/RSDKv4 built on 8/20/24, along with the mods Sonic Forever and Sonic 2 Absolute.
---
+++ soniccd/Sonic CD.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+get_controls
+
+# Source Device Info
+source $controlfolder/device_info.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+
+# Set variables
+GAMEDIR="/$directory/ports/soniccd"
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+# Set current virtual screen
+if [ "$CFW_NAME" == "muOS" ]; then
+  /opt/muos/extra/muxlog & CUR_TTY="/tmp/muxlog_info"
+elif [ "$CFW_NAME" == "TrimUI" ]; then
+  CUR_TTY="/dev/fd/1"
+else
+  CUR_TTY="/dev/tty0"
+fi
+
+cd $GAMEDIR
+
+# Exports
+LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$GAMEDIR/libs"
+
+# Setup gl4es environment
+if [ -f "${controlfolder}/libgl_${CFW_NAME}.txt" ]; then 
+  source "${controlfolder}/libgl_${CFW_NAME}.txt"
+else
+  source "${controlfolder}/libgl_default.txt"
+fi
+
+# Permissions
+$ESUDO chmod 666 /dev/tty0
+$ESUDO chmod 666 /dev/tty1
+$ESUDO chmod 777 $GAMEDIR/soniccd
+
+# Modify ScreenWidth
+LOW=214 # 3:2
+MED=320 # 4:3
+HIGH=426 # 16:9
+
+# Set WIDTH based on DISPLAY_WIDTH
+case $DISPLAY_WIDTH in
+  [0-2][0-9][0-9])  # 0 to 299 range
+    WIDTH=$LOW
+    ;;
+  [3-9][0-9][0-9])  # 300 to 999 range
+    WIDTH=$MED
+    ;;
+  [1-9][0-9][0-9][0-9])  # 1000 and above range
+    WIDTH=$HIGH
+    ;;
+  *)
+    echo "Unknown screen width: $DISPLAY_WIDTH"
+    WIDTH=$MED  # Default value or handle as needed
+    ;;
+esac
+
+if grep -q "^ScreenWidth=[0-9]\+" "$GAMEDIR/settings.ini"; then
+    sed -i "s/^ScreenWidth=[0-9]\+/ScreenWidth=$WIDTH/" "$GAMEDIR/settings.ini"
+else
+    echo "Possible invalid or missing settings.ini!"
+fi
+
+# Run the game
+echo "Loading, please wait!" > $CUR_TTY
+$GPTOKEYB "soniccd" -c "sonic.gptk" &
+SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+./soniccd
+
+$ESUDO kill -9 $(pidof gptokeyb)
+$ESUDO systemctl restart oga_events &
+printf "\033c" > /dev/tty1
